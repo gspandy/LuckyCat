c0a2f606e01e4a11100095bb7bb66765358fbe47
Jesse6789 <sweetdream-fly@hotmail.com>
2017-05-23 09:53:35 +0800

    基类添加常用数据结构

--- src/main/java/com/jesse/controller/index/BaseController.java
+++ src/main/java/com/jesse/controller/index/BaseController.java
@@ -6,13 +6,22 @@ import com.jesse.mapper.ResourcesMapper;
 import com.jesse.plugin.PageView;
 import com.jesse.util.Common;
 import com.jesse.util.FormMap;
+import com.jesse.util.core.metatype.Dto;
+import com.jesse.util.core.metatype.impl.BaseDto;
+import com.jesse.util.core.util.BL3Utils;
+import com.jesse.util.core.web.WebUtils;
+import net.sf.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 
@@ -21,6 +30,15 @@ import java.util.List;
  * date：2014-2-17
  */
 public class BaseController {
+	@Autowired
+	private HttpServletRequest request;
+
+	private Class<?> clazz; // 对应的实体
+	protected Map<String, Object> map = new HashMap<String, Object>();
+	protected Dto params = new BaseDto();
+	protected String code;
+	protected String msg;
+	protected JSONObject result=new JSONObject();//封装接口固定结构
 	@Inject
 	private ResourcesMapper resourcesMapper;
 	
@@ -237,5 +255,64 @@ public class BaseController {
 			e.printStackTrace();
 		}
 		return t;
+	}
+	public Class<?> getClazz() {
+		return clazz;
+	}
+
+	public void setClazz(Class<?> clazz) {
+		this.clazz = clazz;
+	}
+
+	public Map<String, Object> getMap() {
+		return map;
+	}
+
+	public void setMap(Map<String, Object> map) {
+		this.map = map;
+	}
+
+	public Dto getParams() {
+		return params;
+	}
+
+	public void setParams(Dto params) {
+		this.params = params;
+	}
+
+	public HttpServletRequest getRequest() {
+		return request;
+	}
+
+	public void setRequest(HttpServletRequest request) {
+		this.request = request;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	@ModelAttribute
+	public void setReqAndRes(HttpServletRequest request) {
+		this.request = request;
+		this.params = WebUtils.getParamAsDto(this.request);
+		BL3Utils.mapRemoveNull(this.params);
+	}
+	public JSONObject getResult(){
+		result.put("code",code);
+		result.put("msg",msg);
+		return result;
 	}
 }
